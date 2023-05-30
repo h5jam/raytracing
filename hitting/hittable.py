5e9fa69659e07d541b8c0576be657d610d3fd0dd
h5jam <so.h4ns@gmail.com>
2023-05-30 10:44:36 +0900

    add: face normal

--- hitting/hittable.py
+++ hitting/hittable.py
@@ -5,9 +5,14 @@ from utils.ray import *
 
 @dataclass
 class hit_record:
-    p: point3 = None
-    normal: vec3 = None
-    t: float = None
+    p: point3
+    normal: vec3
+    t: float
+    front_face: bool
+
+    def set_face_normal(self, r, outward_normal):
+        self.front_face = r.dir().dot(outward_normal) < 0
+        self.normal = outward_normal if self.front_face else -outward_normal
 
 
 class hittable(metaclass=ABCMeta):
